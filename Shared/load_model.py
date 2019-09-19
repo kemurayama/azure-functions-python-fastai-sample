ff6ef38606ae3d85c363ee03a19e743ae03c3f0f
yoshiomura <anotherkindofgreen.m.k@gmail.com>
2019-09-19 19:12:50 +0900

    debug passed

--- Shared/load_model.py
+++ Shared/load_model.py
@@ -1,10 +1,15 @@
 from fastai.basic_train import load_learner, defaults
 from torch import device
-import aiohttp
-import asyncio
+from pathlib import Path
+import os
+import logging
+
 
 def load_fastai():
+    modelpath = Path(__file__).parent
+    modelpath = modelpath/'trained'
+    logging.info('load model path {0}'.format(os.getcwd()))
     defaults.device = device('cpu')
-    learn = load_learner('trained')
+    learn = load_learner(modelpath)
     learn.load('stage-2')
     return learn
